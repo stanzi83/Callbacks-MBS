036d59765a4a0dc016253bc377d250c114404a52
Michele Squires <m.stanzi@gmail.com>
2015-07-17 16:14:21 -0600

    answers

---
+++ Class practice.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Michele on 7/16/2015.
+ */
+class Ferrari {
+    constructor(color, hasLeather){
+       //var this - {};
+
+        //return this
+    }
+}
+
+var ferrariInstance = new Ferrari('blue', true);
--- callbackPractice.js
+++ callbackPractice.js
@@ -25,12 +25,17 @@ and what you should write is the favNum function that makes the code above work,
 
 
   //Code Here for first
+
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var first = function(arr, cb) {
+    cb(arr[0]);
+};
+
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
 });
-
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
@@ -41,38 +46,44 @@ first(names, function(firstName){
   //Code Here for last
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var last = function(arr, cb) {
+    cb(arr[arr.length - 1]);
+};
+
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
 
 
   //Code Here for multiply
+var multiply = function(num1, num2,cb){
+    cb(num1 * num2);
+};
 
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
-})
-
-
+});
 
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
-
-
   //Code Here for contains
+var contains = function(arr, name, cb){
+    var isPresent = false;
+    arr.forEach(function(input, i){
+        if(input === name){
+            isPresent = true;
+        }
+    });
+    cb(isPresent);
+};
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -84,16 +95,60 @@ contains(names, 'Colt', function(result){
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
     //Code Here for uniq
+var uniq = function(arr, cb){
+    for(var i = arr.length - 1; i>= 0; i--){
+        for(var j = arr.length - 2; j >= 0; j--){
+            if(arr[i] === arr[j]){
+                arr.splice(j, 1);
+                i--;
+            }
+        }
+    }
+    cb(arr);
+};
 
+for (var i = 0; i < 10; i++){
+    debugger;
+}
+
+
+var uniq = function(arr, cb) {
+    var newArr = [];
+    arr.forEach(function(input, i) {
+        if (newArr.length === 0) {
+            newArr.push(input);
+        } else {
+            var inArray = false;
+            newArr.forEach(function (input2, i) {
+                if (input === input2) {
+                    inArray = true;
+                }
+            });
+            if (inArray === false) {
+                newArr.push(input);
+            }
+        }
+    });
+    cb(newArr);
+};
+
+
+
+var uniq = function(arr, cb){
+    for(var i = arr.length - 1; i>= 0; i--){
+        for(var j = arr.length - 2; j >= 0; j--){
+            if(arr[i] === arr[j]){
+                arr.splice(j, 1);
+                i--;
+                j--;
+            }
+        }
+    }
+    cb(arr);
+};
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -109,12 +164,25 @@ uniq(names, function(uniqArr){
 
 
     //Code Here for each
-
+var contains = function(arr, name, cb){
+    var isPresent = false;
+    arr.forEach(function(input, i){
+        if(input === name){
+            isPresent = true;
+        }
+    });
+    cb(isPresent);
+};
+var each;
+each = function (arr, cb) {
+    arr.forEach(function (input, i) {
+       cb(arr[i], i)
+    });
+};
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
-
 
 
 
@@ -151,3 +219,30 @@ var users = [
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
+
+
+Review
+
+var doThreeThings = function(cb1, cb2, cb3){
+    setTimeout(function(){
+        var result1 = cb1;
+        setTimeout(function(){
+            var result2 = cb2(result1)
+            setTimeout(function(){
+                cb3(result3)
+            }, 300)
+        });
+    }
+
+
+doThreeThings(
+    function() {
+        return 10;
+    },
+    function(lastResult) {
+        return lastResult + 10;
+    },
+    function(lastResult) {
+        console.log(lastResult)
+    }
+)};
